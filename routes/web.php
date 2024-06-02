dd506cad8ee66e74ad937dda0a6a29f450db1671
DucAnh <anhnguyen2k373@gmail.com>
2024-06-02 19:54:50 +0700

    edit route

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,22 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\UserController;
+
+
+
+// ===FE ROUTE===========================================================================
 
 Route::get('/', [LoginController::class, 'index'])->name('index');
 Route::get('/register', [UserController::class, 'register'])->name('register');
 Route::get('/login', [UserController::class, 'login'])->name('login');
-Route::post('/store', [UserController::class, 'store'])->name('store');
-Route::post('/storeLogin', [UserController::class, 'storeLogin'])->name('storeLogin');
 Route::get('/logout', [UserController::class, 'logout'])->name('logout');
 Route::get('/account', [UserController::class, 'account'])->name('account');
 Route::get('/logout_up', [UserController::class, 'logout_up'])->name('logout_up');
+
+Route::post('/store', [UserController::class, 'store'])->name('store');
+Route::post('/storeLogin', [UserController::class, 'storeLogin'])->name('storeLogin');
 
 
 
