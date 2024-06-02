1605e57b3e24b5185a7dfdb7711e5c4f1916564e
DucAnh <anhnguyen2k373@gmail.com>
2024-06-02 19:53:05 +0700

    edit route

--- routes/web.php
+++ routes/web.php
@@ -5,17 +5,6 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\UserController;
 
-
-// Route::get('/', function () {
-//     return view('register');
-// });
-
-// Route::post('/register', [HomeController::class, 'register'])->name('register');
-// Route::post('/login', [HomeController::class, 'login'])->name('login');
-// Route::get('/account', [HomeController::class, 'account']);
-// Route::post('/logout', [HomeController::class, 'logout'])->name('logout');
-
-
 Route::get('/', [LoginController::class, 'index'])->name('index');
 Route::get('/register', [UserController::class, 'register'])->name('register');
 Route::get('/login', [UserController::class, 'login'])->name('login');
@@ -25,8 +14,5 @@ Route::get('/logout', [UserController::class, 'logout'])->name('logout');
 Route::get('/account', [UserController::class, 'account'])->name('account');
 Route::get('/logout_up', [UserController::class, 'logout_up'])->name('logout_up');
 
-// Route::post('/login', [LoginController::class, 'validateUser'])->name('login');
-// Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
-Route::get('/content', [LoginController::class, 'checkLogin']);
-// Route::post('/register', [LoginController::class, 'register'])->name('register');
+
 
