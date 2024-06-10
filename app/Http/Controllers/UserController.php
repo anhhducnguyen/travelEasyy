4548f5172c2bd194bc901164aceadfadf4013b6d
DucAnh <anhnguyen2k373@gmail.com>
2024-06-10 08:58:25 +0700

    login with google2

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -23,17 +23,29 @@ class UserController extends Controller
     {
         return view('fe.account');
     }
+    // public function store(Request $req)
+    // {
+    //     $req->merge(['password'=>bcrypt($req->password)]);
+    //     try {
+    //         User::create($req->all());
+    //     } catch (\Throwable $th){
+    //         // dd($th);
+    //     }
+    //     return redirect()->route('index');
+    // }
     public function store(Request $req)
     {
-        $req->merge(['password'=>bcrypt($req->password)]);
+        $req->merge(['password' => bcrypt($req->password)]);
         try {
             User::create($req->all());
-        } catch (\Throwable $th){
-            // dd($th);
+            session()->flash('showLoginModal', true);
+        } catch (\Throwable $th) {
+            // Handle error
+            // session()->flash('error', 'Registration failed.');
         }
         return redirect()->route('index');
-        // return view('fe.login');
     }
+
     public function storeLogin(Request $req)
     {
        if(Auth::attempt(['email'=>$req->email,'password'=>$req->password]))
