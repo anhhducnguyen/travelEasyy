a8dd2b7d9775b9e0dfb15a84ba7eebe01764e319
DucAnh <anhnguyen2k373@gmail.com>
2024-06-16 12:11:43 +0700

    change file

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,6 +11,8 @@ use Illuminate\Notifications\Notifiable;
 class User extends Authenticatable
 {
     use HasFactory, Notifiable;
+    protected $table = 'users';
+    protected $primaryKey = 'id';
     protected $fillable = [
         'name', 
         'email', 
