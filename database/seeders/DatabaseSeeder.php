fd8e1c55137afadd5aa7f5289ce5d4cb1a8182a9
vuminhphong23 <phongvuminh2003@gmail.com>
2024-06-19 01:55:03 +0700

    edit seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,37 +14,37 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        User::factory(10)->create();
 
-        // User::factory()->create([
-        //     'name' => 'Admin',
-        //     'email' => 'admin@gmail.com',
-        //     'password' => '1234',
-        //     'role' => 'admin',
-        // ]);
-        // User::factory()->create([
-        //     'name' => 'phong vu',
-        //     'email' => 'user@gmail.com',
-        //     'password' => '1234',
-        //     'email_verified_at' => now(),
-        //     'role' => 'user',
-        // ]);
         User::factory()->create([
-            'name' => 'Duc Anh',
-            'email' => '21012478@st.phenikaa-uni.edu.vn',
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'password' => '1234',
+            'role' => 'admin',
+        ]);
+        User::factory()->create([
+            'name' => 'phong vu',
+            'email' => 'user@gmail.com',
             'password' => '1234',
             'email_verified_at' => now(),
             'role' => 'user',
         ]);
-
-        // $this->call([
-        //     AddressSeeder::class,
-        //     VehicleSeeder::class,
-        //     TourGuideSeeder::class,
-        //     HotelSeeder::class,    
-        //     TourSeeder::class,
-        //     BookingSeeder::class,
-        //     // Add more seeders here if needed
+        // User::factory()->create([
+        //     'name' => 'Duc Anh',
+        //     'email' => '21012478@st.phenikaa-uni.edu.vn',
+        //     'password' => '1234',
+        //     'email_verified_at' => now(),
+        //     'role' => 'user',
         // ]);
+
+        $this->call([
+            AddressSeeder::class,
+            VehicleSeeder::class,
+            TourGuideSeeder::class,
+            HotelSeeder::class,    
+            TourSeeder::class,
+            BookingSeeder::class,
+            // Add more seeders here if needed
+        ]);
     }
 }
