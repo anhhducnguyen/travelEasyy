b5ccfa75c154a81f52a4fd7df1a1b16d224fb22f
vuminhphong23 <phongvuminh2003@gmail.com>
2024-06-21 21:34:26 +0700

    edit blog

--- app/Http/Controllers/User/UserBlogController.php
+++ app/Http/Controllers/User/UserBlogController.php
@@ -11,9 +11,15 @@ class UserBlogController extends Controller
 {
     public function index()
     {
+        $now = Carbon::now();
         $bookings = Booking::with(['user', 'tour'])
                 ->where('confirmation_status', 'confirmed')
                 ->where('payment_status', 'paid')
+                ->whereIn('tour_id', function($query) use ($now) {
+                    $query->select('id')
+                          ->from('tours')
+                          ->where('endDay', '<', $now);
+                })
                 ->get();
         return view('fe.blog.blog', compact('bookings'));
     }
